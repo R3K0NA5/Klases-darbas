4c5f3b78b78195822c91a834642a88d17f0d45b2
R3K0NA5 <donatas.gallente@gmail.com>
2022-12-12 15:09:08 +0200

    First commit

---
+++ js/mano scriptas.js
@@ -0,0 +1,32 @@
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(z);
+
+let a = 1;
+ console.log(a);
+
+
+let b = "Donatas Gelumbauskas ";
+let c = "Mano Vardas ";
+console.log(c+b)
+
+
+let p = 0;
+let o = 0;
+let i = 2;
+let metai = "199" + p;
+let menuo = "1" + o;
+let diena = "2" + i;
+let r = p + o + i;
+console.log(r);
+
+/*let metai = 1990;
+let menuo = 10;
+let diena = 22;
+let liekanametai = metai % 10;
+let liekanamenuo = metai % 10;
+let liekanadiena = metai % 10;*/
+
+
+
